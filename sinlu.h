41fab7918833d0dbd0a28b6d318f1aa2c612c2fb
Seungwoo Han <swoohan@kakao.com>
2022-10-23 17:25:17 +0900

    Create sinlu.h

---
+++ sinlu.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <torch/torch.h>
+#include <torch/nn.h>
+
+namespace sinlu{
+typedef struct SinLUImpl : torch::nn::Module {
+    SinLUImpl(int64_t N)
+    {
+        a = register_parameter("a", torch::ones(N), true);
+        b = register_parameter("b", torch::ones(N), true);
+    }
+    torch::Tensor forward(torch::Tensor x) {
+        return torch::sigmoid(x)*(x+a*torch::sin(b*x));
+        }
+    torch::Tensor a, b;
+    }SinLUImpl;
+    TORCH_MODULE(SinLU);
+}
