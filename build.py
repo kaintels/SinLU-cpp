83e2ca720dd0bc1f7dbf072d818f6968e0b68af2
Seungwoo Han <swoohan@kakao.com>
2022-10-23 18:46:08 +0900

    Update build.py

--- build.py
+++ build.py
@@ -3,5 +3,11 @@ import os
 
 current_dir = os.getcwd()
 
-cmd = "cmake --build " +current_dir+"/build --config Release --target ALL_BUILD -j 14 --"
+cmd = "cmake --build "+\
+      current_dir+\
+      "/build --config Release --target ALL_BUILD -j 14 --"
+
 subprocess.run(cmd, shell=True)
+
+# for i in range(5):
+#     subprocess.run("./build/Release/Main.exe")
