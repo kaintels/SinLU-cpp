362b945e7029ef471eff6ba8e51fc6bdc3f3e664
Seungwoo Han <swoohan@kakao.com>
2022-10-23 18:38:58 +0900

    Create build.py

---
+++ build.py
@@ -0,0 +1,7 @@
+import subprocess
+import os
+
+current_dir = os.getcwd()
+
+cmd = "cmake --build " +current_dir+"/build --config Release --target ALL_BUILD -j 14 --"
+subprocess.run(cmd, shell=True)
